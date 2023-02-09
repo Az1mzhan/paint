8e724c23f78ec4e30239cf99e472f42c2f5392a6
Az1mzhan <78950175+Az1mzhan@users.noreply.github.com>
2023-02-09 19:09:48 +0600

    Delete Paint directory

--- Paint/scripts/paletteRenderMode.js
+++
@@ -1,6 +0,0 @@
-const paletteRenderMode = {
-    SCREEN: 'screen',
-    PHONE: 'phone'
-}
-
-export default paletteRenderMode;
--- Paint/scripts/random.js
+++
@@ -1,5 +0,0 @@
-// The function returns a pseudorandom number up to max number (mx)
-const getRandomInt = (mx) => {
-    return Math.floor(Math.random() * mx);
-}
-export default getRandomInt
